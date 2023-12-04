f69c08e9316eccb0e2e0740b804f0aba204c99da
Thore Goll <thore.goll@franka.de>
2023-12-04 20:20:24 +0100

    [03] Solution B

--- src/bin/third.rs
+++ src/bin/third.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, fs, str::FromStr};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+    str::FromStr,
+};
 
 use aoc23::Part;
 use clap::Parser;
@@ -18,6 +22,7 @@ struct Options {
 #[derive(Debug, Default)]
 struct Schematic {
     symbols: HashMap<Coord, char>,
+    gears: HashSet<Coord>,
     numbers: HashMap<Coord, u32>,
 }
 
@@ -34,11 +39,16 @@ impl From<char> for CharKind {
         match c {
             '0'..='9' => CharKind::Digit,
             '.' => CharKind::Ignore,
-            // '#' => CharKind::Symbol,
-            // _ => CharKind::Ignore,
             _ => CharKind::Symbol,
         }
     }
+}
+
+fn neighbors(c: Coord) -> impl Iterator<Item = Coord> {
+    ((c.x - 1)..=(c.x + 1))
+        .cartesian_product((c.y - 1)..=(c.y + 1))
+        .map(|(x, y)| Coord::new(x, y))
+        .filter(move |n| *n != c)
 }
 
 impl FromStr for Schematic {
@@ -47,6 +57,7 @@ impl FromStr for Schematic {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut symbols = HashMap::new();
         let mut numbers = HashMap::new();
+        let mut gears = HashSet::new();
         let _ = s
             .lines()
             .enumerate()
@@ -61,25 +72,30 @@ impl FromStr for Schematic {
                         CharKind::Ignore => {}
                         CharKind::Symbol => {
                             let (x, symbol) = group.next().expect("Symbol");
-                            let (x, y) = (x as i32, y as i32);
-                            symbols.extend(
-                                ((x - 1)..=(x + 1))
-                                    .cartesian_product((y - 1)..=(y + 1))
-                                    .map(|(x, y)| (Coord::new(x, y), symbol)),
-                            );
+                            let c = Coord::new(x as i32, y as i32);
+                            symbols.extend(neighbors(c).map(|c| (c, symbol)));
+                            if symbol == '*' {
+                                gears.insert(c);
+                            }
                         }
                         CharKind::Digit => {
                             let (x, a) = group.next().expect("Number");
                             let mut s = String::from(a);
                             s.extend(group.map(|(_, c)| c));
-                            let value = s.parse().expect(&format!("Valid number, not {s}"));
+                            let value = s
+                                .parse()
+                                .unwrap_or_else(|_| panic!("Valid number, not {s}"));
                             numbers.insert(Coord::new(x as i32, y as i32), value);
                         }
                     }
                 }
             })
             .collect::<Vec<_>>();
-        Ok(Schematic { numbers, symbols })
+        Ok(Schematic {
+            numbers,
+            symbols,
+            gears,
+        })
     }
 }
 
@@ -94,15 +110,30 @@ impl Schematic {
             })
             .map(|(_, n)| *n)
     }
+
+    fn gear_ratios(&self) -> impl Iterator<Item = (u32, u32)> + '_ {
+        self.gears.iter().filter_map(|gc| {
+            self.numbers
+                .iter()
+                .filter(|(nc, num)| {
+                    neighbors(*gc)
+                        .cartesian_product(
+                            (0..format!("{num}").len()).map(|x| **nc + Coord::new(x as i32, 0)),
+                        )
+                        .any(|(gc, nc)| gc == nc)
+                })
+                .map(|(_, num)| *num)
+                .next_tuple()
+        })
+    }
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Options::parse();
+    let schematic = Schematic::from_str(&fs::read_to_string(&args.input)?)?;
     let solution = match args.part {
-        Part::One => Schematic::from_str(&fs::read_to_string(&args.input)?)?
-            .numbers_touching_symbol()
-            .sum::<u32>(),
-        Part::Two => todo!(),
+        Part::One => schematic.numbers_touching_symbol().sum::<u32>(),
+        Part::Two => schematic.gear_ratios().map(|(a, b)| a * b).sum::<u32>(),
     };
     println!("Solution part {:?}: {solution}", args.part);
     Ok(())
@@ -120,6 +151,19 @@ mod tests {
             Schematic::from_str(input)
                 .expect("Schematic FromStr")
                 .numbers_touching_symbol()
+                .sum::<u32>()
+        )
+    }
+
+    #[test]
+    fn sample_part_two() {
+        let input = include_str!("../../sample/third.txt");
+        assert_eq!(
+            467835,
+            Schematic::from_str(input)
+                .expect("Schematic FromStr")
+                .gear_ratios()
+                .map(|(a, b)| a * b)
                 .sum::<u32>()
         )
     }
