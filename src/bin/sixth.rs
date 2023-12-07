347499a455a00dd2bfb50f07298b623042ca09f3
Thore Goll <thore.goll@franka.de>
2023-12-07 12:40:24 +0100

    [06] Solution B

--- src/bin/sixth.rs
+++ src/bin/sixth.rs
@@ -1,5 +1,3 @@
-use std::str::FromStr;
-
 use aoc23::Part;
 
 use anyhow::anyhow;
@@ -7,9 +5,10 @@ use clap::Parser;
 use itertools::izip;
 use nom::{
     bytes::complete::tag,
-    character::complete::{newline, space1, u32},
-    multi::separated_list1,
-    sequence::{preceded, separated_pair, tuple},
+    character::complete::{digit1, newline, space0, space1, u64},
+    combinator::{map, peek},
+    multi::{many_till, separated_list1},
+    sequence::{preceded, separated_pair, terminated, tuple},
     Finish, IResult, Parser as NomParser,
 };
 
@@ -27,12 +26,8 @@ struct Options {
 fn main() -> anyhow::Result<()> {
     let args = Options::parse();
     let input = std::fs::read_to_string(args.input)?;
-    let races = Document::from_str(&input)?;
-
-    let solution = match args.part {
-        Part::One => races.margin(),
-        Part::Two => unimplemented!(),
-    };
+    let races = Document::parse(&input, args.part)?;
+    let solution = races.margin();
     println!("Solution part {part:?}: {solution}", part = args.part);
 
     Ok(())
@@ -40,11 +35,11 @@ fn main() -> anyhow::Result<()> {
 
 #[derive(Debug, PartialEq, Eq)]
 struct Race {
-    time: u32,
-    distance: u32,
+    time: u64,
+    distance: u64,
 }
 impl Race {
-    fn new(time: u32, distance: u32) -> Self {
+    fn new(time: u64, distance: u64) -> Self {
         Self { time, distance }
     }
 }
@@ -52,14 +47,17 @@ impl Race {
 #[derive(Debug)]
 struct Document(Vec<Race>);
 
-impl FromStr for Document {
-    type Err = anyhow::Error;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(parse_races(s).finish().map_err(|e| anyhow!("{e}"))?.1)
-    }
-}
-
 impl Document {
+    fn parse(s: &str, part: Part) -> anyhow::Result<Self> {
+        let parser = match part {
+            Part::One => parse_list_of_numbers,
+            Part::Two => parse_single_number,
+        };
+        Ok(parse_races(s, parser)
+            .finish()
+            .map_err(|e| anyhow!("{e}"))?
+            .1)
+    }
     fn margin(&self) -> usize {
         self.0
             .iter()
@@ -75,14 +73,24 @@ impl Race {
     }
 }
 
-fn parse_races(s: &str) -> IResult<&str, Document> {
+fn parse_list_of_numbers(s: &str) -> IResult<&str, Vec<u64>> {
+    separated_list1(space1, u64)(s)
+}
+fn parse_single_number(s: &str) -> IResult<&str, Vec<u64>> {
+    map(
+        many_till(terminated(digit1, space0), peek(newline)),
+        |(digits, _)| vec![digits.join("").parse::<u64>().unwrap()],
+    )(s)
+}
+
+fn parse_races<'a, P>(s: &'a str, numbers: P) -> IResult<&str, Document>
+where
+    P: NomParser<&'a str, Vec<u64>, nom::error::Error<&'a str>> + Clone,
+{
     separated_pair(
-        preceded(tuple((tag("Time:"), space1)), separated_list1(space1, u32)),
+        preceded(tuple((tag("Time:"), space1)), numbers.clone()),
         newline,
-        preceded(
-            tuple((tag("Distance:"), space1)),
-            separated_list1(space1, u32),
-        ),
+        preceded(tuple((tag("Distance:"), space1)), numbers),
     )
     .map(|(times, distances)| {
         izip!(times, distances)
@@ -101,7 +109,7 @@ mod tests {
 
     #[rstest]
     #[case(Race::new(7, 9), &[(2,10), (3,12), (4,12), (5,10)])]
-    fn sample_a_individual(#[case] race: Race, #[case] expectations: &[(u32, u32)]) {
+    fn sample_a_individual(#[case] race: Race, #[case] expectations: &[(u64, u64)]) {
         for (i, (expected, actual)) in expectations
             .iter()
             .map(|(t, d)| Race::new(*t, *d))
@@ -115,7 +123,15 @@ mod tests {
     #[test]
     fn sample_a() {
         let input = include_str!("../../sample/sixth.txt");
-        let races = Document::from_str(input).expect("parsing");
+        let races = Document::parse(input, Part::One).expect("parsing");
         assert_eq!(288, races.margin());
+    }
+
+    #[test]
+    fn sample_b() {
+        let input = include_str!("../../sample/sixth.txt");
+        let races = Document::parse(input, Part::Two).expect("parsing");
+        assert_eq!(vec![Race::new(71530, 940200)], races.0);
+        assert_eq!(71503, races.margin());
     }
 }
