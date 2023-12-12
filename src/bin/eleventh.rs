fb40fab630711592f152724120c9ff2f21afc7f9
Thore Goll <thore.goll@franka.de>
2023-12-12 12:58:19 +0100

    [11] Solution B

--- src/bin/eleventh.rs
+++ src/bin/eleventh.rs
@@ -1,11 +1,11 @@
-use std::{fmt::Debug, str::FromStr};
+use std::{collections::BTreeSet, fmt::Debug, str::FromStr};
 
 use aoc23::Part;
 
 use clap::Parser;
 use euclid::Vector2D;
 use itertools::Itertools;
-use ndarray::{concatenate, prelude::*};
+use ndarray::prelude::*;
 
 /// Day 10: Pipe Maze
 #[derive(Debug, Parser)]
@@ -28,14 +28,15 @@ fn main() -> anyhow::Result<()> {
 
     let mut universe = Universe::from_str(&input)?;
 
-    universe.expand();
-    let solution = match args.part {
-        Part::One => universe
-            .shortest_paths()
-            .map(|(_, _, dist)| dist)
-            .sum::<i32>(),
-        Part::Two => unimplemented!(),
-    };
+    universe.expand(match args.part {
+        Part::One => 2,
+        Part::Two => 1_000_000,
+    });
+
+    let solution = universe
+        .shortest_paths()
+        .map(|(_, _, dist)| dist)
+        .sum::<i64>();
 
     if args.verbose {
         println!("{universe:?}");
@@ -47,45 +48,19 @@ fn main() -> anyhow::Result<()> {
 #[derive(Default, PartialEq, Eq)]
 struct Universe {
     sky: Array2<char>,
-    route: Route,
+    expansion: usize,
+    horizontal: BTreeSet<i64>,
+    vertical: BTreeSet<i64>,
 }
 
-#[derive(Debug, Default, PartialEq, Eq)]
-struct Route {
-    path: Vec<Coord>,
-    cost: i32,
-}
-
-type Coord = Vector2D<i32, euclid::UnknownUnit>;
-type Coordf = Vector2D<f32, euclid::UnknownUnit>;
-
-fn is_on_line(c: Coord, a: Coord, b: Coord) -> bool {
-    let a = Coordf::new(a.x as f32, a.y as f32);
-    let b = Coordf::new(b.x as f32, b.y as f32);
-    let c = Coordf::new(c.x as f32, c.y as f32);
-    let d = (a - c).length() + (c - b).length() - (a - b).length();
-    d.abs() < 0.12
-}
+type Coord = Vector2D<i64, euclid::UnknownUnit>;
 
 impl Debug for Universe {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let shape = self.sky.shape();
         for y in 0..shape[0] {
             for x in 0..shape[1] {
-                let c = Coord::new(y as i32, x as i32);
-                let item = self.sky[[y, x]];
-                let is_on_line = self
-                    .route
-                    .path
-                    .iter()
-                    .copied()
-                    .tuple_windows()
-                    .any(|(a, b)| is_on_line(c, a, b));
-                if item != GALAXY && is_on_line {
-                    write!(f, "{ROUTE}")?;
-                } else {
-                    write!(f, "{item}")?;
-                }
+                write!(f, "{}", self.sky[[y, x]])?;
             }
             writeln!(f)?;
         }
@@ -94,72 +69,44 @@ impl Debug for Universe {
 }
 
 impl Universe {
-    #[allow(dead_code)]
-    fn dimension(&self) -> (usize, usize) {
-        (self.sky.ncols(), self.sky.nrows())
-    }
-    fn add_row(&mut self, i: usize) {
-        let row = Array2::from_elem((1, self.sky.shape()[1]), '┈');
-        self.sky = concatenate![
-            Axis(0),
-            self.sky.slice(s![..i, ..]),
-            row,
-            self.sky.slice(s![i.., ..])
-        ];
-    }
-
-    fn add_col(&mut self, i: usize) {
-        let col = Array2::from_elem((self.sky.shape()[0], 1), '┊');
-        self.sky = concatenate![
-            Axis(1),
-            self.sky.slice(s![.., ..i]),
-            col,
-            self.sky.slice(s![.., i..])
-        ];
-    }
-
-    fn expand(&mut self) {
-        let row_insertions = self
+    fn expand(&mut self, factor: usize) {
+        self.expansion = factor - 1;
+        self.vertical = self
             .sky
             .axis_iter(Axis(0))
             .enumerate()
             .filter(|(_, row)| row.iter().all(|c| *c != GALAXY))
-            .map(|(i, _)| i)
-            .collect::<Vec<_>>();
+            .map(|(i, _)| i as i64)
+            .collect::<BTreeSet<_>>();
 
-        for (offset, i) in row_insertions.into_iter().enumerate() {
-            self.add_row(i + offset);
-        }
-
-        let col_insertions = self
+        self.horizontal = self
             .sky
             .axis_iter(Axis(1))
             .enumerate()
             .filter(|(_, col)| col.iter().all(|c| *c != GALAXY))
-            .map(|(i, _)| i)
-            .collect::<Vec<_>>();
-
-        for (offset, i) in col_insertions.into_iter().enumerate() {
-            self.add_col(i + offset);
-        }
+            .map(|(i, _)| i as i64)
+            .collect::<BTreeSet<_>>();
     }
 
-    fn manhattan(a: &Coord, b: &Coord) -> i32 {
+    fn manhattan(&self, a: &Coord, b: &Coord) -> i64 {
+        let (start, end) = (a.min(*b), a.max(*b));
+        let eh = self.horizontal.range(start.x..end.x).count() * self.expansion;
+        let ev = self.vertical.range(start.y..end.y).count() * self.expansion;
         let d = (*a - *b).abs();
-        d.x + d.y
+        d.x + d.y + eh as i64 + ev as i64
     }
 
     fn galaxies(&self) -> impl Iterator<Item = Coord> + '_ + Clone {
         self.sky
             .indexed_iter()
             .filter(|(_, c)| **c == GALAXY)
-            .map(|((x, y), _)| Coord::new(x as i32, y as i32))
+            .map(|((x, y), _)| Coord::new(y as i64, x as i64))
     }
 
-    fn shortest_paths(&self) -> impl Iterator<Item = (Coord, Coord, i32)> + '_ {
+    fn shortest_paths(&self) -> impl Iterator<Item = (Coord, Coord, i64)> + '_ {
         self.galaxies()
             .tuple_combinations()
-            .map(|(a, b)| (a, b, Self::manhattan(&a, &b)))
+            .map(|(a, b)| (a, b, self.manhattan(&a, &b)))
     }
 }
 
@@ -185,9 +132,8 @@ impl FromStr for Universe {
     }
 }
 
-const VOID: char = ' ';
+const VOID: char = '·';
 const GALAXY: char = '●';
-const ROUTE: char = '·';
 
 #[cfg(test)]
 mod tests {
@@ -196,21 +142,20 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    fn sample_a() {
+    #[case(2, 374)]
+    #[case(10, 1030)]
+    #[case(100, 8410)]
+    fn sample(#[case] expansion: usize, #[case] expected_path_len: i64) {
         let input = include_str!("../../sample/eleventh.txt");
         let mut universe = Universe::from_str(input).expect("parsing");
 
-        assert_eq!((10, 10), universe.dimension(), "{universe:?}");
-
-        universe.expand();
-        println!("{universe:?}");
-        assert_eq!((13, 12), universe.dimension(), "{universe:?}");
+        universe.expand(expansion);
         assert_eq!(
-            374,
+            expected_path_len,
             universe
                 .shortest_paths()
                 .map(|(_, _, dist)| dist)
-                .sum::<i32>(),
+                .sum::<i64>(),
             "{universe:?}"
         );
     }
