96b20a77e986ba33780aee5638685d1f76bba2e1
Thore Goll <thoregoll@googlemail.com>
2023-12-22 12:07:21 +0100

    [15] Solution B

--- src/bin/fifteenth.rs
+++ src/bin/fifteenth.rs
@@ -1,9 +1,17 @@
-use std::hash::Hasher;
+use std::{collections::HashMap, hash::Hasher, iter::repeat};
 
 use anyhow::Result;
-use aoc23::Part;
+use aoc23::{anyhowing, Part};
 use clap::Parser;
 use derive_more::{Add, AsRef, From, Into, Sum};
+use itertools::izip;
+use nom::{
+    character::complete::{alpha1, char, digit1},
+    multi::separated_list1,
+    sequence::tuple,
+    Finish, IResult, Parser as NomParser,
+};
+use nom_supreme::ParserExt;
 
 /// Day 15: Lens Library
 #[derive(Debug, Parser)]
@@ -26,20 +34,110 @@ fn main() -> Result<()> {
             .map(|line| {
                 line.split(',')
                     .map(|chunk| chunk.bytes().collect::<HASH>())
-                    .map(|hash| *hash.as_ref() as usize)
-                    .sum::<usize>()
+                    .map(|hash| *hash.as_ref() as u64)
+                    .sum::<u64>()
             })
-            .sum::<usize>(),
-        Part::Two => unimplemented!(),
+            .sum::<u64>(),
+        Part::Two => {
+            let facility = Facility::from_str(&input)?;
+            facility.focal_power()
+        }
     };
     println!("Solution part {:?}: {solution}", args.part);
 
     Ok(())
 }
 
+type Label<'a> = &'a str;
+type FocalLength = u64;
+type Box<'a> = Vec<(Label<'a>, FocalLength)>;
+type Instruction<'a> = (Label<'a>, Operation);
+
+struct Facility<'a>(HashMap<u8, Box<'a>>);
+
+impl<'a> FromIterator<Instruction<'a>> for Facility<'a> {
+    fn from_iter<T: IntoIterator<Item = Instruction<'a>>>(iter: T) -> Self {
+        Self(
+            iter.into_iter()
+                .fold(HashMap::new(), |mut map, (label, operation)| {
+                    // println!("Label {label} ({}): {operation:?}", hash(label));
+                    match operation {
+                        Operation::Remove => {
+                            if let Some(box_) = map.get_mut(&hash(label)) {
+                                box_.retain(|lens| lens.0 != label);
+                            }
+                        }
+                        Operation::Insert(fl) => {
+                            let box_ = map.entry(hash(label)).or_default();
+                            match box_.iter_mut().find(|(l, _)| label == *l) {
+                                Some(lens) => lens.1 = fl,
+                                None => box_.push((label, fl)),
+                            }
+                        }
+                    };
+                    map
+                }),
+        )
+    }
+}
+
+impl<'a> Facility<'a> {
+    fn from_str(s: &'a str) -> Result<Self> {
+        Ok(instructions(s)
+            .finish()
+            .map_err(anyhowing)?
+            .1
+            .into_iter()
+            .collect())
+    }
+
+    fn focal_power(&self) -> u64 {
+        self.0
+            .iter()
+            .flat_map(|(box_, lenses)| {
+                izip!(repeat(1 + *box_ as u64), 1.., lenses)
+                    .map(|(box_nr, slot, (_, focal_length))| box_nr * slot * focal_length)
+                // .inspect(|x| println!(">> {x:?}"))
+            })
+            .sum()
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum Operation {
+    Remove,
+    Insert(FocalLength),
+}
+
+fn operation(s: &str) -> IResult<&str, Operation> {
+    char('-')
+        .value(Operation::Remove)
+        .or(char('=')
+            .precedes(digit1.map_res(str::parse))
+            .map(Operation::Insert))
+        .parse(s)
+}
+
+fn label(s: &str) -> IResult<&str, Label<'_>> {
+    alpha1(s)
+}
+fn instruction(s: &str) -> IResult<&str, (Label<'_>, Operation)> {
+    tuple((label, operation)).parse(s)
+}
+
+fn instructions(s: &str) -> IResult<&str, Vec<(Label<'_>, Operation)>> {
+    separated_list1(char(','), instruction).parse(s)
+}
+
 #[derive(Debug, Default, PartialEq, Eq, From, Into, Add, Sum, AsRef)]
 #[allow(clippy::upper_case_acronyms)]
 struct HASH(u8);
+
+fn hash(s: &str) -> u8 {
+    let mut h = HASH::default();
+    h.write(s.as_bytes());
+    h.finish() as u8
+}
 
 impl Hasher for HASH {
     fn finish(&self) -> u64 {
@@ -101,5 +199,21 @@ mod tests {
                     .sum::<u64>())
                 .sum::<u64>()
         );
+    }
+
+    #[rstest]
+    #[case("rn=1", Ok(("",("rn", Operation::Insert(1)))))]
+    #[case("cm-", Ok(("",("cm", Operation::Remove))))]
+    #[case("qp=3", Ok(("",("qp", Operation::Insert(3)))))]
+    #[case("foobar=3,blub", Ok((",blub",("foobar", Operation::Insert(3)))))]
+    fn sample_b_parsing(#[case] input: &str, #[case] expected: IResult<&str, (&str, Operation)>) {
+        assert_eq!(expected, instruction(input));
+    }
+
+    #[rstest]
+    fn sample_b() {
+        let input = include_str!("../../sample/fifteenth.txt");
+        let facility = Facility::from_str(input).expect("parsing");
+        assert_eq!(145, facility.focal_power());
     }
 }
